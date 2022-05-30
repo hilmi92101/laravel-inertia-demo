239bf29db44c4ddb46740cde7a4766be27b6ae5e
hilmi <muhammadhilmi92101@gmail.com>
2022-05-30 21:11:03 +0800

    initial commit

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,17 @@
+<template>  
+    <div> 
+        <div v-for="post in posts">
+            <p>{{ post.title }}</p>
+            <p>{{ post.content }}</p>
+            <hr>
+        </div>
+    </div>  
+</template>  
+<script>  
+    export default {  
+        props: {
+            posts: Object
+        }
+          
+    }  
+</script>
