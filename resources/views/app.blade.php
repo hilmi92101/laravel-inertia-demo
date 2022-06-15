13da6684dbd68bf13be81fe4c429f5c332081d87
hilmi <muhammadhilmi92101@gmail.com>
2022-06-15 19:11:31 +0800

    refactor sidebar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,6 +23,8 @@
         <link href="/asset/dashboard4/assets/css/tooltips.css" rel="stylesheet" />
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
         <!-- ------------------------------------------------------
             ALL
         ------------------------------------------------------ -->
